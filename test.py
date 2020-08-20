e7241d20d44f8176ef5d305e97d2fe8549ae528c
midnight4ever <hyouka266@gmail.com>
2020-08-20 21:14:46 +0700

    update new branch

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import io
 # url = 'http://127.0.0.1:8080/predictions/cartoonize'
 url = 'https://cartoonize-pvowi6uvjq-as.a.run.app/predictions/cartoonize'
 
-path = 'cartoonizer/test_images/china6.jpg'
+path = 'cartoonizer/test_images/mountain4.jpg'
 f = open(path, "rb")
 image = Image.open(f)
 byte_array = io.BytesIO()
