6ecde5d0f39efe6ee87f8f99716c318ede773bc1
midnight4ever <hyouka266@gmail.com>
2020-08-25 01:46:44 +0700

    change from binary to base64 image

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import requests
 from PIL import Image
 import io
+import base64
 
-# url = 'http://127.0.0.1:8080/predictions/cartoonize'
-url = 'https://cartoonize-pvowi6uvjq-as.a.run.app/predictions/cartoonize'
-path = 'cartoonizer/test_images/mountain4.jpg'
+url = 'http://127.0.0.1:8080/predictions/cartoonize'
+# url = 'https://cartoonize-pvowi6uvjq-as.a.run.app/predictions/cartoonize'
+path = '../kitten.jpg'
 
 f = open(path, "rb")
 image = Image.open(f)
@@ -13,5 +14,7 @@ image.save(byte_array, format='jpeg')
 byte_array = byte_array.getvalue()
 
 res = requests.post(url, data=byte_array)
-img = Image.open(io.BytesIO(res.content))
-img.save('test.jpg')
+imgdata = base64.b64decode(res.text)
+filename = 'test.jpg'
+with open(filename, 'wb') as f:
+    f.write(imgdata)
